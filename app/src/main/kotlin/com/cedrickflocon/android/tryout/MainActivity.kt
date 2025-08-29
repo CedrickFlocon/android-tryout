359bea720aa985e33e0bc6a4111b319dd48206fd
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2025-08-29 10:41:20 +0200

    focusable & clickable

--- app/src/main/kotlin/com/cedrickflocon/android/tryout/MainActivity.kt
+++ app/src/main/kotlin/com/cedrickflocon/android/tryout/MainActivity.kt
@@ -3,14 +3,24 @@ package com.cedrickflocon.android.tryout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicText("Hello world")
+            BasicText(
+                "Hello world",
+                modifier = Modifier
+                    .onFocusChanged { println(it) }
+                    .focusable()
+                    .clickable { println("HELLO") }
+            )
         }
     }
 
