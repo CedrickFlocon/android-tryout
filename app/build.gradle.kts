eeaa6d3d596e7e5290b6ce611f7164f10f62112e
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2025-09-08 16:43:54 +0200

    target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     compileSdk = libs.versions.android.compile.sdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.min.sdk.get().toInt()
+        targetSdk = libs.versions.android.target.sdk.get().toInt()
         versionName = "1.0.0"
         versionCode = 1
     }
--- app/src/main/kotlin/com/cedrickflocon/android/tryout/MainActivity.kt
+++ app/src/main/kotlin/com/cedrickflocon/android/tryout/MainActivity.kt
@@ -3,14 +3,19 @@ package com.cedrickflocon.android.tryout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicText("Hello world")
+            Box(modifier = Modifier.safeContentPadding()) {
+                BasicText("Hello world")
+            }
         }
     }
 
